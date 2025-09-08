e28026be9cbe37a830cf3ce34dde07fb1183d0bc
deepika5791 <deepikatripathi0810@gmail.com>
2025-09-08 12:43:17 +0530

    added frontend port

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const cors = require("cors");
 const moviesRouter = require("./src/routes/movieroutes");
 const app = express();
 const bodyParser = require("body-parser");
-app.use(cors())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(bodyParser.json());
 app.use("/movies", moviesRouter);
 
