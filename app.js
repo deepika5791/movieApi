7c9651d98546cc905038910b883d424a3fa0e546
deepika5791 <deepikatripathi0810@gmail.com>
2025-09-08 12:10:48 +0530

    added new package

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const moviesRouter = require("./src/routes/movieroutes");
 const app = express();
 const bodyParser = require("body-parser");
+app.use(cors())
 app.use(bodyParser.json());
 app.use("/movies", moviesRouter);
 
