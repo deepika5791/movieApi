5e8c56d173a70bd0aa5169b0a08feb55b6216d95
deepika5791 <deepikatripathi0810@gmail.com>
2025-09-29 01:24:09 +0530

    correcrd some code

--- main.js
+++ main.js
@@ -5,11 +5,7 @@ const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose
-  .connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-
+  .connect(MONGO_URI)
   .then(() => {
     console.log("MongoDB Connect");
     app.listen(PORT, "0.0.0.0", () => {
